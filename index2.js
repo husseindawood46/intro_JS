2728f75f8192aac91713c87f1ccb21445c171dfa
hussein Dawood <140665430+husseindawood46@users.noreply.github.com>
2024-08-20 20:08:34 +0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,17 @@
+let x = 10;
+if (x >= 10) {
+  console.log("x>=10");
+} else if (x >= 5) {
+  console.log("x>=5");
+} else {
+  console.log("x<5");
+}
+
+let arr = [10, 20, 30, 40, 50];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+function fullName(firstName, LastName) {
+  console.log(firstName + " " + LastName);
+}
+fullName("Hussein", "Mohamed");
